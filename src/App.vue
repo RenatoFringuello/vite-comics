f6b97d4da28b62780fdaaf81d2b50d37981ce829
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-09 17:40:58 +0100

    add content section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
   import AppHeader from './components/AppHeader.vue';
+  import AppContent from './components/AppContent.vue';
 </script>
 
 <template>
   <AppHeader />
+  <main>
+    <AppContent />
+  </main>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppContent.vue
@@ -0,0 +1,23 @@
+<script>
+   
+</script>
+
+<template>
+    <section class="sans-narrow">
+        <div class="container">
+            <h1>--&gt;Content goes here&lt--</h1>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+    @use '../style/partials/variables' as *;
+
+    section{
+        background-color: $content-bg;
+        color:white;
+        .container{
+            padding: 2.5rem 0;
+        }
+    }
+</style>
