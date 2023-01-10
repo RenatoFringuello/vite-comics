8447d25b7a071bc5d8e35e0266e3d08e623a79aa
RenatoFringuello <renatofringuello@hotmail.it>
2023-01-10 18:30:27 +0100

    add book img width height

--- src/components/AppContentBook.vue
+++ src/components/AppContentBook.vue
@@ -1,6 +1,7 @@
 <script>
     export default {
         props : {
+            nBookPerRow: Number,
             thumb: String,
             price: String,
             series: String,
@@ -15,9 +16,14 @@
 </script>
 
 <template>
-    <div @mouseenter="isHover = true" @mouseleave="isHover = false" class="card">
+    <div 
+        @mouseenter="isHover = true" 
+        @mouseleave="isHover = false" 
+        class="card"
+        :style="`width: calc((100% / ${nBookPerRow}) - 1rem);`"
+        >
         <div class="img-container">
-            <img :src="thumb" :alt="series">
+            <img :style="`height: calc((1200px / ${nBookPerRow}) - 1rem);`" :src="thumb" :alt="series">
         </div>
         <div class="text">{{ series.toUpperCase() }}</div>
         <div class="text" :class="isHover ? 'visible' : 'hidden' ">{{ price }}</div>
@@ -28,7 +34,7 @@
     @use '../style/partials/variables' as *;
     $margin-bottom:1rem;
     div.card{
-        width: calc((100% / 6) - 1rem);
+        // width: calc((100% / 6) - 1rem);
         padding-top :1rem;
         cursor: pointer;
         
@@ -36,7 +42,7 @@
             margin-bottom: $margin-bottom;
             img{
                 width: 100%;
-                height: 178px;
+                // height: calc((1200px / 6) - 1rem);
                 object-fit: cover;
                 object-position:top;
                 display: block;
